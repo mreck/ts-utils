55a3403ac24ef0fd169cfd1e271a3306464b5aa5
Mathias Reck <mathias.reck@gmail.com>
2025-07-25 09:29:51 +0200

    Turn results into classes

--- src/result.test.ts
+++ src/result.test.ts
@@ -1,64 +1,58 @@
-import {
-  catchAsyncResult,
-  coerceResultError,
-  isError,
-  isSuccess,
-  newResultSuccess,
-  unwrapResult,
-} from "./result";
+import { catchAsyncResult, ResultError, ResultSuccess } from "./result";
 
-describe("result", () => {
-  const s = newResultSuccess("s");
-  const e = coerceResultError("e");
+test("ResultError", () => {
+  const e = ResultError.coerce("e");
 
-  test("isSuccess()", () => {
-    expect(isSuccess(s)).toBe(true);
-    expect(isSuccess(e)).toBe(false);
+  expect(e.isError()).toBe(true);
+  expect(e.isSuccess()).toBe(false);
+  expect(() => e.unwrap()).toThrow("e");
+});
+
+test("ResultSuccess", () => {
+  const s = new ResultSuccess("s");
+  const sss = new ResultSuccess(new ResultSuccess(new ResultSuccess("s")));
+
+  expect(s.isError()).toBe(false);
+  expect(s.isSuccess()).toBe(true);
+  expect(s.unwrap()).toBe("s");
+  expect(sss.flatten()).toEqual(s);
+});
+
+describe("catchAsyncResult", () => {
+  const s = new ResultSuccess("s");
+  const e = ResultError.coerce("e");
+
+  let sfn = 0;
+  let efn = 0;
+  const sf = () => {
+    ++sfn;
+    return "s";
+  };
+  const ef = () => {
+    ++efn;
+    throw "e";
+  };
+
+  test("without reties", async () => {
+    sfn = 0;
+    efn = 0;
+
+    expect(await catchAsyncResult(sf)).toEqual(s);
+    expect(sfn).toEqual(1);
+
+    expect(await catchAsyncResult(ef)).toEqual(e);
+    expect(efn).toEqual(1);
   });
 
-  test("isError()", () => {
-    expect(isError(s)).toBe(false);
-    expect(isError(e)).toBe(true);
-  });
+  test("with reties", async () => {
+    sfn = 0;
+    efn = 0;
+    const opts = { retries: 3 };
 
-  test("unwrapResult()", () => {
-    expect(unwrapResult(s)).toEqual("s");
-    expect(() => unwrapResult(e)).toThrow("e");
-  });
+    expect(await catchAsyncResult(sf, opts)).toEqual(s);
+    expect(sfn).toEqual(1);
 
-  describe("catchAsyncResult", () => {
-    let sfn = 0;
-    let efn = 0;
-    const sf = () => {
-      ++sfn;
-      return "s";
-    };
-    const ef = () => {
-      ++efn;
-      throw "e";
-    };
-
-    test("without reties", async () => {
-      sfn = 0;
-      efn = 0;
-
-      expect(await catchAsyncResult(sf)).toEqual(s);
-      expect(sfn).toEqual(1);
-
-      expect(await catchAsyncResult(ef)).toEqual(e);
-      expect(efn).toEqual(1);
-    });
-
-    test("with reties", async () => {
-      sfn = 0;
-      efn = 0;
-      const opts = { retries: 3 };
-
-      expect(await catchAsyncResult(sf, opts)).toEqual(s);
-      expect(sfn).toEqual(1);
-
-      expect(await catchAsyncResult(ef, opts)).toEqual(e);
-      expect(efn).toEqual(4);
-    });
+    expect(await catchAsyncResult(ef, opts)).toEqual(e);
+    expect(efn).toEqual(4);
   });
 });
--- src/result.ts
+++ src/result.ts
@@ -1,59 +1,72 @@
 import { coerceError } from "./error";
 
-export interface ResultError {
-  _t: -1;
-  error: Error;
-}
-
-export interface ResultSuccess<T> {
-  _t: 1;
-  data: T;
-}
-
-export type Result<T> = ResultError | ResultSuccess<T>;
-
-export type AsyncResult<T> = Promise<ResultError | ResultSuccess<T>>;
-
-export function isError<T>(result: Result<T>): result is ResultError {
-  return result._t === -1;
-}
-
-export function isSuccess<T>(result: Result<T>): result is ResultSuccess<T> {
-  return result._t === 1;
-}
-
-export function unwrapResult<T>(res: Result<T>) {
-  if (isError(res)) {
-    throw res.error;
+class ResultBase<T> {
+  isError(): this is ResultError<T> {
+    return this instanceof ResultError;
   }
-  return res.data;
+
+  isSuccess(): this is ResultSuccess<T> {
+    return this instanceof ResultSuccess;
+  }
+
+  unwrap(): T {
+    if (this.isSuccess()) {
+      return this.data;
+    }
+    if (this.isError()) {
+      throw this.error;
+    }
+    throw new Error("can not unwrap ResultBase");
+  }
 }
 
-export function newResultError(error: Error): ResultError {
-  return { _t: -1, error };
+export class ResultError<T> extends ResultBase<T> {
+  constructor(readonly error: Error) {
+    super();
+  }
+
+  static coerce<T>(value: unknown): ResultError<T> {
+    return new ResultError(coerceError(value));
+  }
 }
 
-export function newResultSuccess<T>(data: T): ResultSuccess<T> {
-  return { _t: 1, data };
+export class ResultSuccess<T> extends ResultBase<T> {
+  constructor(readonly data: T) {
+    super();
+  }
+
+  flatten(): ResultSuccess<T> {
+    if (this.data instanceof ResultSuccess) {
+      return this.data.flatten();
+    }
+    return this;
+  }
 }
 
-export function coerceResultError(value: unknown): ResultError {
-  return newResultError(coerceError(value));
+export type Result<T> = ResultError<T> | ResultSuccess<T>;
+
+export type AsyncResult<T> = Promise<ResultError<T> | ResultSuccess<T>>;
+
+export function isResult<T>(val: unknown): val is Result<T> {
+  return val instanceof ResultBase;
 }
 
 export async function catchAsyncResult<T>(
-  fn: () => T,
+  fn: () => T | Result<T> | AsyncResult<T>,
   opts?: { retries?: number },
 ): AsyncResult<T> {
   const retries = Math.max(0, opts?.retries ?? 0);
-  let result: Result<T> = coerceResultError("out of retries");
+  let result: Result<T> = ResultError.coerce("out of retries");
 
   for (let i = 0; i <= retries; i++) {
     try {
-      result = newResultSuccess(await fn());
-      break;
+      const r = await fn();
+      result = isResult(r) ? r : new ResultSuccess(r);
+      if (result.isSuccess()) {
+        break;
+      }
     } catch (error) {
-      result = coerceResultError(error);
+      result = ResultError.coerce(error);
     }
   }
 
