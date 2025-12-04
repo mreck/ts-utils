7c825a13db06d9d05976e861ff78db48a8133f8f
Mathias Reck <mathias.reck@gmail.com>
2025-12-05 00:40:50 +0100

    feat: extend ExtSet

--- src/extset.test.ts
+++ src/extset.test.ts
@@ -1,18 +1,62 @@
 import { ExtSet } from "./extset";
 
+const expectExtSetValues = <T>(s: ExtSet<T>, values: T[]) =>
+  expect(s.getValues().sort()).toEqual(values);
+
+test("toggle()", () => {
+  const s = new ExtSet<number>([1, 2, 3]);
+
+  s.toggle(2);
+  expectExtSetValues(s, [1, 3]);
+
+  s.toggle(2);
+  expectExtSetValues(s, [1, 2, 3]);
+});
+
 test("getValues()", () => {
   const s = new ExtSet<number>();
 
   s.add(1).add(2).add(2).add(1).add(3);
-  expect(s.getValues().sort()).toEqual([1, 2, 3]);
+  expectExtSetValues(s, [1, 2, 3]);
+});
+
+test("hasAny()", () => {
+  const s = new ExtSet<number>([1, 2, 3, 4]);
+
+  expect(s.hasAny([3, 5])).toBe(true);
+  expect(s.hasAny([5, 6])).toBe(false);
+});
+
+test("hasAll()", () => {
+  const s = new ExtSet<number>([1, 2, 3, 4]);
+
+  expect(s.hasAll([3, 4])).toBe(true);
+  expect(s.hasAll([4, 5])).toBe(false);
 });
 
 test("addMany()", () => {
   const s = new ExtSet<number>();
 
   s.addMany([1, 2, 1, 3, 4, 2, 1, 5]);
-  expect(s.getValues().sort()).toEqual([1, 2, 3, 4, 5]);
+  expectExtSetValues(s, [1, 2, 3, 4, 5]);
 
   s.addMany([1, 3, 5, 7, 9]);
-  expect(s.getValues().sort()).toEqual([1, 2, 3, 4, 5, 7, 9]);
+  expectExtSetValues(s, [1, 2, 3, 4, 5, 7, 9]);
+});
+
+test("deleteMany()", () => {
+  const s = new ExtSet<number>([1, 2, 3, 4, 5, 6, 7]);
+
+  s.deleteMany([1, 3, 5, 7]);
+  expectExtSetValues(s, [2, 4, 6]);
+});
+
+test("toggleMany()", () => {
+  const s = new ExtSet<number>([1, 2, 3]);
+
+  s.toggleMany([3, 4]);
+  expectExtSetValues(s, [1, 2, 4]);
+
+  s.toggleMany([3, 4]);
+  expectExtSetValues(s, [1, 2, 3]);
 });
--- src/extset.ts
+++ src/extset.ts
@@ -1,11 +1,52 @@
 export class ExtSet<T> extends Set<T> {
+  toggle(value: T): this {
+    if (this.has(value)) {
+      this.delete(value);
+    } else {
+      this.add(value);
+    }
+    return this;
+  }
+
   getValues(): T[] {
     return Array.from(this.values());
   }
 
-  addMany(values: T[]): ExtSet<T> {
+  hasAny(values: T[]): boolean {
+    for (const value of values) {
+      if (this.has(value)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  hasAll(values: T[]): boolean {
+    for (const value of values) {
+      if (!this.has(value)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  addMany(values: T[]): this {
     for (const value of values) {
       this.add(value);
+    }
+    return this;
+  }
+
+  deleteMany(values: T[]): this {
+    for (const value of values) {
+      this.delete(value);
+    }
+    return this;
+  }
+
+  toggleMany(values: T[]): this {
+    for (const value of values) {
+      this.toggle(value);
     }
     return this;
   }
